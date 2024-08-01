c500240512fcaf95c2ce4f7145384d09b124bfbb
NathiPrakash17 <manitamiltkv1997@gmail.com>
2024-08-01 14:30:40 +0530

    Update checker.js - translation

--- checker.js
+++ checker.js
@@ -1,44 +1,49 @@
-function isTemperatureInRange(temperature) {
-    const isInRange = temperature >= 0 && temperature <= 45;
-    if (!isInRange) {
-        console.log("Temperature is out of range!");
-    }
-    return isInRange;
+let currentLanguage = 'english';
+let translations = {};
+
+function loadLanguage(lang) {
+  fetch('translations.json')
+    .then(response => response.json())
+    .then(data => {
+      if (data[lang]) {
+        currentLanguage = lang;
+        translations = data;
+      } else {
+        console.error(`Language ${lang} not supported`);
+      }
+    })
+    .catch(error => {
+      console.error('Error loading translations:', error);
+    });
 }
 
-function isSocInRange(soc) {
-    const isInRange = soc >= 20 && soc <= 80;
-    if (!isInRange) {
-        console.log("State of Charge is out of range!");
-    }
-    return isInRange;
+function getTranslation(key) {
+  return translations[currentLanguage][key];
 }
 
-function isChargeRateInRange(chargeRate) {
-    const isInRange = chargeRate <= 0.8;
-    if (!isInRange) {
-        console.log("Charge Rate is out of range!");
-    }
-    return isInRange;
+function isInRange(value, min, max) {
+  const isInRange = value >= min && value <= max;
+  if (!isInRange) {
+    console.log(getTranslation(`${value} is out of range!`)); 
+  }
+  return isInRange;
 }
 
 function batteryIsOk(temperature, soc, chargeRate) {
-    const temperatureIsOk = isTemperatureInRange(temperature);
-    const socIsOk = isSocInRange(soc);
-    const chargeRateIsOk = isChargeRateInRange(chargeRate);
+  const temperatureIsOk = isInRange(temperature, 0, 45);
+  const socIsOk = isInRange(soc, 20, 80);
+  const chargeRateIsOk = isInRange(chargeRate, 0, 0.8);
 
-    return temperatureIsOk && socIsOk && chargeRateIsOk;
+  return temperatureIsOk && socIsOk && chargeRateIsOk;
 }
 
 function main() {
-    let batteryOk = batteryIsOk(25, 70, 0.7);
+  loadLanguage('german'); 
+  let batteryOk = batteryIsOk(25, 70, 0.7);
 
-    if (batteryOk) {
-        console.log("All ok");
-    } else {
-        console.log("Battery is not ok");
-    }
+  if (batteryOk) {
+    console.log(getTranslation('allOk'));
+  } else {
+    console.log(getTranslation('batteryNotOk'));
+  }
 }
-
-main();
-
