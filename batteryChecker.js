74dd324f1e52766fa78f87eb074e50ff1ab40d9b
NathiPrakash17 <manitamiltkv1997@gmail.com>
2024-08-08 13:53:15 +0530

    Create batteryChecker.js

---
+++ batteryChecker.js
@@ -0,0 +1,14 @@
+import { TranslationService } from './translationService';
+import { batteryIsOk } from './batteryChecker';
+import { log } from './logger';
+
+const translationService = new TranslationService();
+
+async function main() {
+  await translationService.setLanguage('german');
+  const batteryOk = batteryIsOk(25, 70, 0.7);
+
+  log(batteryOk ? translationService.getTranslation('allOk') : translationService.getTranslation('batteryNotOk'));
+}
+
+main();
